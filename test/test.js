a25160a977da00024fe0acf67c646f3143070ebf
Facundo Victor <facundovt@gmail.com>
2016-03-10 19:18:08 -0300

    Add a test-case

--- test/test.js
+++ test/test.js
@@ -1,6 +1,13 @@
 var assert = require('assert');
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+
 describe('Array', function() {
+
   describe('#indexOf()', function () {
+
+    it('This is just a pending test');
+
     it('should return -1 when the value is not present', function () {
       assert.equal(-1, [1,2,3].indexOf(5));
       assert.equal(-1, [1,2,3].indexOf(0));
