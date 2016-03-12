e4da9190d2ec25b224cbea17676f24de07767395
Facundo Victor <facundovt@gmail.com>
2016-03-12 12:33:14 -0300

    Add shouldJS support

--- test/init.js
+++ test/init.js
@@ -2,6 +2,7 @@ assert = require('assert');
 chai = require("chai");
 chaiAsPromised = require("chai-as-promised");
 jsf = require('json-schema-faker');
+should = require('should');
 
 // Load schemas
 mocks = require('./mocks');
