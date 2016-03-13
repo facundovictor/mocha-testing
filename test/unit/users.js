4532ca7d069d433b2494945e4ed42cf1759541c5
Facundo Victor <facundovt@gmail.com>
2016-03-13 18:11:48 -0300

    Set user schema global to the test context

--- test/unit/users.js
+++ test/unit/users.js
@@ -3,12 +3,12 @@ describe('Users', function() {
   describe('Creation', function () {
 
     context('When creating a new user', function() {
+      var some_guy = jsf(mocks.user);
       it('Should have an id', function() {
-        var some_guy = jsf(mocks.user);
-        // ADD SHOULD.JS
-        some_guy.should.have.property('id');
-        some_guy.id.should.be.ok();
-        some_guy.id.should.be.String();
+        some_guy.should.have.property('userId');
+        some_guy.userId.should.be.ok();
+        some_guy.userId.should.be.String();
+      });
       });
     });
   });
