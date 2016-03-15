286906ca5e449736c5b2812e5e4705088fe64d7f
Facundo Victor <facundovt@gmail.com>
2016-03-15 11:12:46 -0300

    Add a ctrl to test editions
    Add a fake controller just for play with the tools.
    Restructure the user test.

--- test/unit/users.js
+++ test/unit/users.js
@@ -1,37 +1,63 @@
 describe('Users', function() {
 
-  describe('Creation', function () {
+  var some_guy, model, ctrl;
 
-    context('When creating a new user', function() {
-      var some_guy = jsf(mocks.user);
-      it('Should have an id', function() {
-        some_guy.should.have.property('userId');
-        some_guy.userId.should.be.ok();
-        some_guy.userId.should.be.String();
-      });
+  beforeEach(function(){
+    sandbox = sinon.sandbox.create();
+    some_guy = jsf(mocks.user);
+    model = {};
 
-      it('Should have a name', function() {
-        some_guy.should.have.property('name'); 
-        some_guy.name.should.be.ok();
-        some_guy.name.should.be.String();
-      });
+    ctrl = {
+      amountOfUsers : 0,
+      current_user : model,
+      editName : function(name){
+        return current_user.save(name);
+      }
+    };
+  });
 
-      it('Shold have a email', function(){
-        some_guy.should.have.property('emailAddr'); 
-        some_guy.emailAddr.should.be.ok();
-        some_guy.emailAddr.should.be.String();
-      });
+  context('When creating a new user', function() {
+    it('Should have an id', function() {
+      some_guy.should.have.property('userId');
+      some_guy.userId.should.be.ok();
+      some_guy.userId.should.be.String();
+    });
 
-      it('The name would be Facu, Robert, or Cesar.', function() {
-        some_guy.name.should.match(/^Facu$|^Robert$|^Cesar$/);
-      });
+    it('Should have a name', function() {
+      some_guy.should.have.property('name');
+      some_guy.name.should.be.ok();
+      some_guy.name.should.be.String();
+    });
 
-      it('The email address should be from the expected domain', function(){
-        some_guy.emailAddr.should.containEql("altoros.com");
-        some_guy.emailAddr.should.match(/@altoros.com$/);
-      });
+    it('Shold have an email', function(){
+      some_guy.should.have.property('emailAddr');
+      some_guy.emailAddr.should.be.ok();
+      some_guy.emailAddr.should.be.String();
+    });
+
+    it('The name would be Facu, Robert, or Cesar.', function() {
+      some_guy.name.should.match(/^Facu$|^Robert$|^Cesar$/);
+    });
+
+    it('The email address should be from the expected domain', function(){
+      some_guy.emailAddr.should.containEql("altoros.com");
+      some_guy.emailAddr.should.match(/@altoros.com$/);
+    });
+  });
+
+  context('When editing an user', function(){
+
+    beforeEach(function(){
+      model['save'] = sandbox.stub();
+      model.save.withArgs(some_guy).returns(some_guy);
+      model.save.withArgs(sinon.match.any).throws("databaseError");
+    });
+
+    it("and this is just for playing", function () {
+
+      // console.log(missionImpossible.assignment("accept", tape));
+      // missionImpossible.assignment("reject", tape);
+
     });
   });
 });
-
-
