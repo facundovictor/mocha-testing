6d05faba0ca92ccf133e95c2ce9001ec40b7c79a
Facundo Victor <facundovt@gmail.com>
2016-03-18 17:36:11 -0300

    Fix chaiAsPromised support

--- test/init.js
+++ test/init.js
@@ -1,11 +1,14 @@
 global.promise = require('bluebird');
 global.assert = require('assert');
+global.jsf = require('json-schema-faker');
+global.sinon = require('sinon');
+
 global.chai = require("chai");
 global.chaiAsPromised = require("chai-as-promised");
-global.jsf = require('json-schema-faker');
-global.should = chai.should();
+global.chai.use(global.chaiAsPromised);
+
 global.expect = chai.expect;
-global.sinon = require('sinon');
+global.should = chai.should();
 global._ = require('lodash');
 
 // Load schemas
